680582953573c9840dcd6f60fc47a39c1d9a5c6c
Kyurenpoto <heal9179@gmail.com>
2021-06-09 21:43:00 +0900

    Set timeout 1 second

--- infra/postclient.py
+++ infra/postclient.py
@@ -10,7 +10,7 @@ from httpx import AsyncClient, Response
 class PostClient(str):
     async def post(self, data: dict[str, Any]) -> dict[str, Any]:
         async with AsyncClient() as client:
-            response: Response = await client.post(url=self, json=data)
+            response: Response = await client.post(url=self, json=data, timeout=1.0)
 
         response.raise_for_status()
 
