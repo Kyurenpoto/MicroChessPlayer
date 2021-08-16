de56848cfabdd978f757e462fb8ca04c04d69e71
Kyurenpoto <heal9179@gmail.com>
2021-08-17 05:56:31 +0900

    Add error response models

--- src/model/responsemodel.py
+++ src/model/responsemodel.py
@@ -54,6 +54,10 @@ class RequestErrorResponseModel(ErrorResponseModel):
     def from_message(cls, message: str) -> RequestErrorResponseModel:
         return RequestErrorResponseModel(message, "request.RequestError")
 
+    @classmethod
+    def from_message_with_service(cls, message: str, service: str) -> BadRequestResponseModel:
+        return BadRequestResponseModel(message, f"{service}.RequestError")
+
 
 class HTTPStatusErrorResponseModel(ErrorResponseModel):
     @classmethod
@@ -61,6 +65,55 @@ class HTTPStatusErrorResponseModel(ErrorResponseModel):
         return HTTPStatusErrorResponseModel(message, "request.HTTPStatusError")
 
 
+class BadRequestResponseModel(ErrorResponseModel):
+    @classmethod
+    def from_message_with_service(cls, message: str, service: str) -> BadRequestResponseModel:
+        return BadRequestResponseModel(message, f"{service}.BadRequest")
+
+
+class PayloadTooLargeResponseModel(ErrorResponseModel):
+    @classmethod
+    def from_message_with_service(cls, message: str, service: str) -> PayloadTooLargeResponseModel:
+        return PayloadTooLargeResponseModel(message, f"{service}.PayloadTooLarge")
+
+
+class UnsupportedMediaTypeResponseModel(ErrorResponseModel):
+    @classmethod
+    def from_message_with_service(cls, message: str, service: str) -> UnsupportedMediaTypeResponseModel:
+        return UnsupportedMediaTypeResponseModel(message, f"{service}.UnsupportedMediaType")
+
+
+class UnprocessableEntityResponseModel(ErrorResponseModel):
+    @classmethod
+    def from_message_with_service(cls, message: str, service: str) -> UnprocessableEntityResponseModel:
+        return UnprocessableEntityResponseModel(message, f"{service}.UnprocessableEntity")
+
+
+class InternalServerErrorResponseModel(ErrorResponseModel):
+    @classmethod
+    def from_message_with_service(cls, message: str, service: str) -> InternalServerErrorResponseModel:
+        return InternalServerErrorResponseModel(message, f"{service}.InternalServerError")
+
+
+class BadGatewayResponseModel(ErrorResponseModel):
+    @classmethod
+    def from_message_with_service(cls, message: str, service: str) -> BadGatewayResponseModel:
+        return BadGatewayResponseModel(message, f"{service}.BadGateway")
+
+
+ResponseErrorResponseModel = Union[
+    BadRequestResponseModel,
+    PayloadTooLargeResponseModel,
+    UnsupportedMediaTypeResponseModel,
+    UnprocessableEntityResponseModel,
+    InternalServerErrorResponseModel,
+    BadGatewayResponseModel,
+]
+
+
 TrajectoryResponsableModel = Union[TrajectoryResponseModel, RequestErrorResponseModel, HTTPStatusErrorResponseModel]
 GameResponsableModel = Union[GameResponseModel, RequestErrorResponseModel, HTTPStatusErrorResponseModel]
 MeasurementResponsableModel = Union[MeasurementResponseModel, RequestErrorResponseModel, HTTPStatusErrorResponseModel]
+NextFENResponsableModel = Union[NextFENResponseModel, RequestErrorResponseModel, HTTPStatusErrorResponseModel]
+NextSANResponsableModel = Union[NextSANResponseModel, RequestErrorResponseModel, HTTPStatusErrorResponseModel]
+FENStatusResponsableModel = Union[FENStatusResponseModel, ErrorResponseModel]
