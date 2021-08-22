ec503b9fa948eec78b88101c69da9f95305db530
Kyurenpoto <heal9179@gmail.com>
2021-08-23 06:13:33 +0900

    Add new components to test utilities

--- src/utility/fakedatafactory.py
+++ src/utility/fakedatafactory.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: MIT
 
 from abc import ABC, abstractmethod
-from typing import Any, Callable, NamedTuple
+from typing import Any, Callable, NamedTuple, Type
 
 from faker import Faker
 
@@ -14,21 +14,21 @@ class FakeDataFactory(ABC):
         pass
 
 
-class ListFactoryData(NamedTuple):
+class ListedFactoryData(NamedTuple):
     origin: FakeDataFactory
     cnt: int
 
 
-class ListFactory(ListFactoryData, FakeDataFactory):
+class ListedFactory(ListedFactoryData, FakeDataFactory):
     def created(self) -> Any:
         return [self.origin.created() for _ in range(self.cnt)]
 
 
-class TupleFactoryData(NamedTuple):
+class TupledFactoryData(NamedTuple):
     origins: list[FakeDataFactory]
 
 
-class TupleFactory(TupleFactoryData, FakeDataFactory):
+class TupledFactory(TupledFactoryData, FakeDataFactory):
     def created(self) -> Any:
         return tuple(origin.created() for origin in self.origins)
 
@@ -40,7 +40,7 @@ class TableFactoryData(NamedTuple):
 
 class TableFactory(TableFactoryData, FakeDataFactory):
     def created(self) -> Any:
-        return ListFactory(TupleFactory(self.origins), self.cnt).created()
+        return ListedFactory(TupledFactory(self.origins), self.cnt).created()
 
 
 class ConditionalFactoryData(NamedTuple):
@@ -79,5 +79,34 @@ class IntegerMetaFactoryData(NamedTuple):
 
 
 class IntegerMetaFactory(IntegerMetaFactoryData, FakeDataMetaFactory):
+    def factory(self, unique: bool = True) -> FakeDataFactory:
+        return IntegerFactory(self.min, self.max, (Faker().unique if unique else Faker()))
+
+
+class ListFactoryData(NamedTuple):
+    value_type: Type
+    factory: Any
+
+
+class ListFactory(ListFactoryData, FakeDataFactory):
+    def created(self) -> Any:
+        return self.factory.pylist(value_types=self.value_type)
+
+
+class ListMetaFactoryData(NamedTuple):
+    value_type: Type
+
+
+class ListMetaFactory(ListMetaFactoryData, FakeDataMetaFactory):
     def factory(self) -> FakeDataFactory:
-        return IntegerFactory(self.min, self.max, Faker().unique)
+        return ListFactory(self.value_type, Faker())
+
+
+class NestedListFactoryData(NamedTuple):
+    inner_factory: FakeDataFactory
+    outer_length_factory: FakeDataFactory
+
+
+class NestedListFactory(NestedListFactoryData, FakeDataFactory):
+    def created(self) -> Any:
+        return [self.inner_factory.created() for _ in range(self.outer_length_factory.created())]
--- src/utility/genericproperty.py
+++ src/utility/genericproperty.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: MIT
 
 from abc import ABC, abstractmethod
-from typing import Any, Callable, NamedTuple
+from typing import Any, Callable, Coroutine, NamedTuple
 
 
 class Property(NamedTuple):
@@ -74,12 +74,12 @@ class Indunction(IndunctionData, GenericProperty):
 
 class BlackboxData(NamedTuple):
     function: Callable[[Any], Any]
-    verifier: Callable[[Any, Any], bool]
+    verifier: Callable[[Any], bool]
 
 
 class Blackbox(BlackboxData, GenericProperty):
     def property(self) -> Property:
-        return Property(lambda x: self.verifier(x, self.function(x)))
+        return Property(lambda x: self.verifier(self.function(x)))
 
 
 class OracleData(NamedTuple):
@@ -90,3 +90,109 @@ class OracleData(NamedTuple):
 class Oracle(OracleData, GenericProperty):
     def property(self) -> Property:
         return Property(lambda x: self.function1(x) == self.function2(x))
+
+
+class AsyncProperty(NamedTuple):
+    property: Callable[[Any], Coroutine[Any, Any, bool]]
+
+    async def verify(self, target: Any) -> None:
+        assert await self.property(target)
+
+
+class AsyncGenericProperty(ABC):
+    @abstractmethod
+    async def property(self) -> AsyncProperty:
+        pass
+
+
+class AsyncSymmetryData(NamedTuple):
+    function: Callable[[Any], Coroutine[Any, Any, Any]]
+    inverse: Callable[[Any], Coroutine[Any, Any, Any]]
+
+
+class AsyncSymmetry(AsyncSymmetryData, AsyncGenericProperty):
+    async def property(self) -> AsyncProperty:
+        async def async_lambda(x) -> bool:
+            return x == await self.function(await self.inverse(x))
+
+        return AsyncProperty(async_lambda)
+
+
+class AsyncCommutativityData(NamedTuple):
+    function1: Callable[[Any], Coroutine[Any, Any, Any]]
+    function2: Callable[[Any], Coroutine[Any, Any, Any]]
+
+
+class AsyncCommutativity(AsyncCommutativityData, AsyncGenericProperty):
+    def property(self) -> AsyncProperty:
+        async def async_lambda(x) -> bool:
+            return await self.function1(await self.function2(x)) == await self.function2(await self.function1(x))
+
+        return AsyncProperty(async_lambda)
+
+
+class AsyncInvariantData(NamedTuple):
+    function: Callable[[Any], Coroutine[Any, Any, Any]]
+    invariant: Callable[[Any], Coroutine[Any, Any, Any]]
+
+
+class AsyncInvariant(AsyncInvariantData, AsyncGenericProperty):
+    def property(self) -> AsyncProperty:
+        async def async_lambda(x) -> bool:
+            return await self.invariant(x) == await self.invariant(await self.function(x))
+
+        return AsyncProperty(async_lambda)
+
+
+class AsyncIdempotenceData(NamedTuple):
+    function: Callable[[Any], Coroutine[Any, Any, Any]]
+
+
+class AsyncIdempotence(AsyncIdempotenceData, AsyncGenericProperty):
+    def property(self) -> AsyncProperty:
+        async def async_lambda(x) -> bool:
+            return await self.function(x) == await self.function(await self.function(x))
+
+        return AsyncProperty(async_lambda)
+
+
+class AsyncIndunctionData(NamedTuple):
+    function: Callable[[Any], Coroutine[Any, Any, Any]]
+    separator: Callable[[Any], Coroutine[Any, Any, list]]
+    composer: Callable[[list], Coroutine[Any, Any, Any]]
+
+
+class AsyncIndunction(AsyncIndunctionData, AsyncGenericProperty):
+    def property(self) -> AsyncProperty:
+        async def async_lambda(x) -> bool:
+            return await self.function(x) == await self.function(
+                await self.composer([await self.function(piece) for piece in await self.separator(x)])
+            )
+
+        return AsyncProperty(async_lambda)
+
+
+class AsyncBlackboxData(NamedTuple):
+    function: Callable[[Any], Coroutine[Any, Any, Any]]
+    verifier: Callable[[Any], Coroutine[Any, Any, bool]]
+
+
+class AsyncBlackbox(AsyncBlackboxData, AsyncGenericProperty):
+    def property(self) -> AsyncProperty:
+        async def async_lambda(x) -> bool:
+            return await self.verifier(await self.function(x))
+
+        return AsyncProperty(async_lambda)
+
+
+class AsyncOracleData(NamedTuple):
+    function1: Callable[[Any], Coroutine[Any, Any, Any]]
+    function2: Callable[[Any], Coroutine[Any, Any, Any]]
+
+
+class AsyncOracle(AsyncOracleData, AsyncGenericProperty):
+    def property(self) -> AsyncProperty:
+        async def async_lambda(x) -> bool:
+            return await self.function1(x) == await self.function2(x)
+
+        return AsyncProperty(async_lambda)
