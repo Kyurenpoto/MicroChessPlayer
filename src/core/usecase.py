c901678732b3ded746e6c03ec4f7ab6039e88f4c
Kyurenpoto <heal9179@gmail.com>
2021-09-15 01:22:51 +0900

    Fix register/resposne logic in class 'Usecase'
    Change signature from tuple to str
    Remove type checking for response

--- src/core/usecase.py
+++ src/core/usecase.py
@@ -5,7 +5,7 @@
 from __future__ import annotations
 
 from abc import abstractmethod
-from typing import Any, NamedTuple, Type, TypeVar, get_args
+from typing import Any, NamedTuple, TypeVar
 
 from src.core.boundary import (
     DataStoreRequestBoundary,
@@ -19,19 +19,17 @@ from src.core.event import EventAGen, PopEvent, PushEvent
 
 class UsecaseData(NamedTuple):
     frameworks: list[FrameworkResponseBoundary]
-    datastores: dict[tuple[Type, Type], DataStoreRequestBoundary]
-    proxies: dict[tuple[Type, Type], ProxyRequestBoundary]
+    datastores: dict[str, DataStoreRequestBoundary]
+    proxies: dict[str, ProxyRequestBoundary]
 
     def register_framework(self, framework: FrameworkResponseBoundary) -> None:
         self.frameworks.append(framework)
 
-    def register_datastore(self, datastore: DataStoreRequestBoundary) -> None:
-        req_type, res_type = get_args(datastore)
-        self.datastores[(req_type, res_type)] = datastore
+    def register_datastore(self, signature: str, datastore: DataStoreRequestBoundary) -> None:
+        self.datastores[signature] = datastore
 
-    def register_proxy(self, proxy: ProxyRequestBoundary) -> None:
-        req_type, res_type = get_args(proxy)
-        self.proxies[(req_type, res_type)] = proxy
+    def register_proxy(self, signature: str, proxy: ProxyRequestBoundary) -> None:
+        self.proxies[signature] = proxy
 
 
 UsecaseReq = TypeVar("UsecaseReq")
@@ -43,9 +41,6 @@ class Usecase(UsecaseData, FrameworkRequestBoundary[UsecaseReq, UsecaseRes], Mul
         return PushEvent(self.executed(request))
 
     async def response(self, response: Any) -> PopEvent:
-        if type(response) not in self.datastores and type(response) not in self.proxies:
-            raise KeyError
-
         return PopEvent(response)
 
     @abstractmethod
